6e44f843855fef3519a596b5ca452fbd4cdac9e8
Martin De Wulf <madewulf@multitasked.net>
2016-08-10 23:46:09 +0200

    Animated lines + bars

--- d3pyramid.js
+++ d3pyramid.js
@@ -1,78 +1,110 @@
-d3.json("belgium2.json", function (error, json) {
+var data;
+var json;
+d3.json("belgium2.json", function (error, j) {
+  json = j;
   if (error) return console.warn(error);
-  data = json;
-  pyramid(data[0].d);
+  data = json[0].d;
+  pyramid();
 });
 
-function pyramid(data) {
-  var width = window.innerWidth,
+var width;
+var maleArr;
+var femaleArr;
+var barHeight;
+var pointCount;
+var sum;
+var femaleX;
+var maleX;
+var chartHeight;
+var data;
+var femaleGenerator;
+var maleGenerator;
+var bar;
+
+function maleWidth(d, i) {
+  var percentage = maleArr[i] / (sum);
+  return maleX(percentage);
+}
+
+function femaleWidth(d, i) {
+  var percentage = (femaleArr[i]) / sum;
+  return femaleX(0.1 - percentage);
+}
+
+function femaleTransform(d, i) {
+  var percentage = femaleArr[i] / sum;
+  return "translate(" + femaleX(percentage) + ",0)";
+}
+
+function pyramid() {
+
+  width = window.innerWidth,
     barHeight = 20;
+  setupData();
 
-  var maleArr = data.m;
-  var femaleArr = data.f;
-
-  data = maleArr;
-  var pointCount = data.length;
-
-  var sum = 0;
-  for (var i = 0; i < pointCount; i++) {
-    sum += maleArr[i] + femaleArr[i];
-  }
 
   /*Bars */
-  var femaleX = d3.scaleLinear()
+  femaleX = d3.scaleLinear()
     .domain([0.1, 0])
     .range([0, width / 2]);
-  var maleX = d3.scaleLinear()
+  maleX = d3.scaleLinear()
     .domain([0, 0.1])
     .range([0, width / 2]);
-  var chartHeight = barHeight * (pointCount + 1);
+  chartHeight = barHeight * (pointCount + 1);
   var chart = d3.select(".chart")
     .attr("width", width)
     .attr("height", barHeight * (pointCount + 1));
 
-  var bar = chart.selectAll("g")
+  bar = chart.selectAll("g")
     .data(data)
     .enter().append("g")
+    .attr('class', "bar")
     .attr("transform", function (d, i) {
       return "translate(0," + (pointCount - 1 - i) * barHeight + ")";
     });
 
-  bar.append("rect")
-    .attr("width", function (d, i) {
-      var percentage = d / (sum);
-      return maleX(percentage);
-    })
-    .attr("height", barHeight - 1)
-    .attr("transform", "translate(" + width / 2 + ",0)")
-    .attr("class", "male");
+   bar.append("rect").attr("class", "male");
+   bar.append("rect").attr("class", "female");
 
-  bar.append("rect")
-    .attr("width", function (d, i) {
-      var percentage = (femaleArr[i]) / sum;
-      return femaleX(0.1 - percentage);
-    })
+  d3.selectAll(".male").attr("width", maleWidth)
     .attr("height", barHeight - 1)
-    .attr("transform", function (d, i) {
-      var percentage = femaleArr[i] / sum;
-      return "translate(" + femaleX(percentage) + ",0)";
-    })
-    .attr("class", "female");
+    .attr("transform", "translate(" + width / 2 + ",0)");
 
+  d3.selectAll(".female")
+    .attr("width", femaleWidth)
+    .attr("height", barHeight - 1)
+    .attr("transform", femaleTransform);
 
   /*Line*/
 
-  var generator = d3.line()
+  femaleGenerator = d3.line()
     .x(function (d, i) {
-      console.log(d, i);
       var percentage = (femaleArr[i]) / sum;
       return maleX(0.1 - percentage);
     })
-    .y(function (d, i) { return chartHeight - (i+1) * barHeight - barHeight/2 });
+    .y(function (d, i) {
+      return chartHeight - (i + 1) * barHeight
+    });
 
   chart.append('path')
     .data([femaleArr])
-    .attr("d", generator)
+    .attr('id', 'femaleLine')
+    .attr("d", femaleGenerator)
+    .attr("class", "popPath");
+
+  maleGenerator = d3.line()
+    .x(function (d, i) {
+      var percentage = (maleArr[i]) / sum;
+      return width / 2 + femaleX(0.1 - percentage);
+    })
+    .y(function (d, i) {
+      return chartHeight - (i + 1) * barHeight
+    });
+
+  chart.append('path')
+    .data([maleArr])
+    .attr('id', 'maleLine')
+    .attr("d", maleGenerator)
     .attr("class", "popPath");
 
   /*Axes */
@@ -84,10 +116,47 @@ function pyramid(data) {
   chart.append("g").attr("transform", "translate(" + width / 2 + "," + bottomY + ")").call(maleXAxis);
   chart.append("g").attr("transform", "translate(0," + bottomY + ")").call(femaleXAxis);
 
-  var yScale =  d3.scaleLinear()
+  var yScale = d3.scaleLinear()
     .domain([100, 0])
     .range([0, barHeight * (pointCount)]);
-  var yAxis  = d3.axisRight().ticks(20).tickPadding(15)
+  var yAxis = d3.axisRight().ticks(20).tickPadding(15)
     .scale(yScale);
   chart.append("g").call(yAxis);
 }
+
+function setupData() {
+  maleArr = data.m;
+  femaleArr = data.f;
+
+  data = maleArr;
+  pointCount = data.length;
+
+  sum = 0;
+  for (var i = 0; i < pointCount; i++) {
+    sum += maleArr[i] + femaleArr[i];
+  }
+}
+
+function changeData() {
+  data = json[100].d;
+  setupData();
+  d3.select("#maleLine").transition().duration(750).attr("d", maleGenerator);
+  d3.select("#femaleLine").transition().duration(750).attr("d", femaleGenerator);
+
+  d3.selectAll(".male")
+    .data(data)
+    .transition().duration(750)
+    .attr("width", maleWidth)
+    .attr("height", barHeight - 1)
+    .attr("transform", "translate(" + width / 2 + ",0)");
+
+  d3.selectAll(".female")
+    .data(data)
+    .transition().duration(750)
+    .attr("width", femaleWidth)
+    .attr("height", barHeight - 1)
+    .attr("transform", femaleTransform);
+}
+
+var button = d3.select("#changeButton");
+button.on('click', changeData)
